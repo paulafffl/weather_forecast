48956127ec919677f84393948683f7a1bb449e0f
Paula <paulafffl@gmail.com>
2024-04-29 21:53:14 +0100

    Add return button

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,10 @@ const App = () => {
     <main className="flex justify-center items-center bg-gradient-to-br from-indigo-400 via-sky-200 to-teal-300 h-[100vh] w-full">
       <section className="bg-white bg-opacity-40 w-[80vw] px-6 py-16 h-auto flex flex-col items-center justify-center text-center rounded drop-shadow-lg">
         {forecast ? (
-          <Forecast forecast={forecast as forecastType} />
+          <Forecast
+            setForecast={setForecast}
+            forecast={forecast as forecastType}
+          />
         ) : (
           <Search setForecast={setForecast} />
         )}
--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,10 +1,14 @@
 import { forecastType } from '../types'
 
 type ForecastProps = {
+  setForecast: React.Dispatch<React.SetStateAction<forecastType | null>>
   forecast: forecastType
 }
 
-const Forecast: React.FC<ForecastProps> = ({ forecast }: ForecastProps) => {
+const Forecast: React.FC<ForecastProps> = ({
+  setForecast,
+  forecast,
+}: ForecastProps) => {
   const Weather = (temp: number): string => `${temp}°`
 
   const today = forecast.list[0]
@@ -44,6 +48,20 @@ const Forecast: React.FC<ForecastProps> = ({ forecast }: ForecastProps) => {
           </div>
         ))}
       </section>
+      <button onClick={() => setForecast(null)}>
+        <svg
+          className="my-3 h-7 w-7 p-0"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path
+            d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"
+            fill="cornflowerblue"
+          />
+        </svg>
+      </button>
     </>
   )
 }
