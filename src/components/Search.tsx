255f93d86de8da7dd53bec0b4bb20b74e9fad37a
Paula <paulafffl@gmail.com>
2024-04-29 13:43:59 +0100

    getLocations API

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,43 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import { locationType } from '../types'
 
-const Search = () => {
+const Search = (): JSX.Element => {
   const [locationInput, setLocationInput] = useState('')
+  const [locationOptions, setLocationOptions] = useState([])
+  const [locationSelected, setLocationSelected] = useState<locationType | null>(
+    null
+  )
+  const API_key = process.env.REACT_APP_API_KEY
+
+  const getLocations = async (value: string) => {
+    try {
+      let response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${API_key}`
+      )
+      if (!response.ok) {
+        throw new Error('failed to fetch')
+      } else {
+        let data = await response.json()
+        setLocationOptions(data)
+      }
+    } catch (error) {
+      console.error('An error ocurred:', error)
+    }
+  }
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    if (value === '') return
+    setLocationInput(value)
+    getLocations(value)
+  }
+
+  const handleClickLocation = (option: locationType) => {
+    setLocationInput(option.name)
+    setLocationSelected(option)
+    setLocationOptions([])
+  }
+
   return (
     <>
       <h1 className="font-thin text-4xl mb-5">
@@ -12,7 +48,7 @@ const Search = () => {
         Enter a location and then select a option from dropdown list
       </p>
 
-      <div className="flex">
+      <div className="flex relative">
         <label htmlFor="location" className={'screen-readers-only'}>
           Location
         </label>
@@ -20,9 +56,23 @@ const Search = () => {
           type="text"
           value={locationInput}
           className="border-2 border-white rounded-l-md p-2 w-3/4"
-          onChange={(e) => setLocationInput(e.target.value)}
+          onChange={(e) => handleChangeInput(e)}
           name="location"
         />
+        {locationOptions.length > 0 && (
+          <ul className="absolute top-11 bg-white ml-1 rounded-b-md">
+            {locationOptions.map((option: locationType, index: number) => (
+              <li
+                key={`${option.name}_${index}`}
+                className="text-left px-2 py-1 hover:bg-teal-100 w-full"
+              >
+                <button onClick={() => handleClickLocation(option)}>
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
         <button
           className="font-bold rounded-r-md border-2 border-white p-2 hover:bg-teal-200"
           onClick={() => alert('search')}
